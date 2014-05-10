ec5e5edaa8a0040b90f3586218b29dba4637b7eb
Nicolaas Matthijs <nicolaas.matthijs@caret.cam.ac.uk>
2014-05-10 19:24:32 +0100

    Multiple repos at same time + More data

--- ui/shared/bd/js/bd.api.github.js
+++ ui/shared/bd/js/bd.api.github.js
@@ -15,25 +15,52 @@
 
 define(['exports', 'jquery'], function(exports, $) {
 
-    /**
-     * [getIssueEventsForRepository description]
-     *
-     * @param  {[type]}   user     [description]
-     * @param  {[type]}   project  [description]
-     * @param  {Function} callback [description]
-     *
-     * @return {[type]}            [description]
-     */
-    var getIssueEventsForRepository = exports.getIssueEventsForRepository = function(user, project, callback) {
-        $.ajax({
-            'url': 'https://api.github.com/repos/' + user + '/' + project + '/issues/events',
-            'method': 'GET',
-            'headers': {
-                'Authorization': 'token ' + require('bd.core').data.token
-            },
-            'success': function(data) {
-                callback(null, data);
+    var getEventsForRepositories = exports.getEventsForRepositories = function(repositories, callback) {
+        var currentRepository = 0;
+        var events = [];
+
+        var getNextRepository = function() {
+            var repository = repositories[currentRepository];
+            getEventsForRepository(repository.split('/')[0], repository.split('/')[1], function(err, repositoryEvents) {
+                if (err) {
+                    return callback(err);
+                }
+
+                // Add the repository events to the overall list of events
+                events = events.concat(repositoryEvents);
+
+                currentRepository++;
+                if (currentRepository === repositories.length) {
+                    // Sort all events chronologically
+                    events.sort(sortEvents);
+                    return callback(null, events);
+                } else {
+                    getNextRepository();
+                }
+            });
+        };
+
+        getNextRepository();
+    };
+
+    var getEventsForRepository = exports.getEventsForRepository = function(user, project, callback) {
+        // Get the pull request comment events
+        getPullCommentEventsForRepository(user, project, function(err, pullCommentEvents) {
+            if (err) {
+                return callback(err);
             }
+
+            // Get the issue events
+            getIssueEventsForRepository(user, project, function(err, issueEvents) {
+                if (err) {
+                    return callback(err);
+                }
+
+                // Merge and sort them chronologically
+                var events = pullCommentEvents.concat(issueEvents);
+                events.sort(sortEvents);
+                return callback(null, events);
+            });
         });
     };
 
@@ -57,14 +84,17 @@ define(['exports', 'jquery'], function(exports, $) {
             'headers': {
                 'Authorization': 'token ' + require('bd.core').data.token
             },
-            'success': function(data) {
-                callback(null, data);
+            'success': function(pullCommentEvents) {
+                $.each(pullCommentEvents, function(pullCommentEventIndex, pullCommentEvent) {
+                    pullCommentEvent.type = 'pull-comment-event';
+                });
+                callback(null, pullCommentEvents);
             }
         });
     };
 
     /**
-     * [getClosedIssues description]
+     * [getIssueEventsForRepository description]
      *
      * @param  {[type]}   user     [description]
      * @param  {[type]}   project  [description]
@@ -72,7 +102,64 @@ define(['exports', 'jquery'], function(exports, $) {
      *
      * @return {[type]}            [description]
      */
-    var getClosedIssues = exports.getClosedIssues = function(user, project, callback) {
+    var getIssueEventsForRepository = exports.getIssueEventsForRepository = function(user, project, callback) {
+        $.ajax({
+            'url': 'https://api.github.com/repos/' + user + '/' + project + '/issues/events',
+            'method': 'GET',
+            'headers': {
+                'Authorization': 'token ' + require('bd.core').data.token
+            },
+            'success': function(issueEvents) {
+                $.each(issueEvents, function(issueEventIndex, issueEvent) {
+                    issueEvent.type = 'issue-event';
+                });
+                callback(null, issueEvents);
+            }
+        });
+    };
+
+    var sortEvents = function(eventA, eventB) {
+        var eventATime = new Date(eventA['created_at']).getTime();
+        var eventBTime = new Date(eventB['created_at']).getTime();
+        return eventATime < eventBTime;
+    };
+
+    var getClosedIssuesForRepositories = exports.getClosedIssuesForRepositories = function(repositories, callback) {
+        var currentRepository = 0;
+        var closedIssues = [];
+
+        var getNextRepository = function() {
+            var repository = repositories[currentRepository];
+            getClosedIssuesForRepository(repository.split('/')[0], repository.split('/')[1], function(err, closedIssuesForRepository) {
+                if (err) {
+                    return callback(err);
+                }
+
+                // Add the repository events to the overall list of events
+                closedIssues = closedIssues.concat(closedIssuesForRepository);
+
+                currentRepository++;
+                if (currentRepository === repositories.length) {
+                    return callback(null, closedIssues);
+                } else {
+                    getNextRepository();
+                }
+            });
+        };
+
+        getNextRepository();
+    };
+
+    /**
+     * [getClosedIssuesForRepository description]
+     *
+     * @param  {[type]}   user     [description]
+     * @param  {[type]}   project  [description]
+     * @param  {Function} callback [description]
+     *
+     * @return {[type]}            [description]
+     */
+    var getClosedIssuesForRepository = exports.getClosedIssuesForRepository = function(user, project, callback) {
         var closedIssues = [];
 
         var getIssues = function(_page, _callback) {
@@ -107,8 +194,34 @@ define(['exports', 'jquery'], function(exports, $) {
         });
     };
 
+    var getOpenIssuesForRepositories = exports.getOpenIssuesForRepositories = function(repositories, callback) {
+        var currentRepository = 0;
+        var openIssues = [];
+
+        var getNextRepository = function() {
+            var repository = repositories[currentRepository];
+            getOpenIssuesForRepository(repository.split('/')[0], repository.split('/')[1], function(err, openIssuesForRepository) {
+                if (err) {
+                    return callback(err);
+                }
+
+                // Add the repository events to the overall list of events
+                openIssues = openIssues.concat(openIssuesForRepository);
+
+                currentRepository++;
+                if (currentRepository === repositories.length) {
+                    return callback(null, openIssues);
+                } else {
+                    getNextRepository();
+                }
+            });
+        };
+
+        getNextRepository();
+    };
+
     /**
-     * [getOpenIssues description]
+     * [getOpenIssuesForRepository description]
      *
      * @param  {[type]}   user     [description]
      * @param  {[type]}   project  [description]
@@ -116,7 +229,7 @@ define(['exports', 'jquery'], function(exports, $) {
      *
      * @return {[type]}            [description]
      */
-    var getOpenIssues = exports.getOpenIssues = function(user, project, callback) {
+    var getOpenIssuesForRepository = exports.getOpenIssuesForRepository = function(user, project, callback) {
         var openIssues = [];
 
         var getIssues = function(_page, _callback) {
